f760a9e446b89ccdc4a7f3320c4ed44a6f34ef46
joelbustoz10@gmail.com <joelbustoz10@gmail.com>
2023-06-22 21:27:30 -0300

    Se modificaron cosas

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,20 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-    suma(15, 20, 30);
-    }
-    public static void suma(int a, int b, int c){
-    int resultado = a+b+c;
-        System.out.println("La suma da un total de "+resultado);
+        suma(15, 20, 30);
         Coche miCoche = new Coche();
         int totalPuertas = miCoche.incrementarPuertas();
         System.out.println("el coche tiene "+ totalPuertas+" puertas");
     }
-    static class Coche{ //mi objeto tiene cuatro puertas y tiene la funcionalidad de sumarla//
+    public static void suma(int a, int b, int c){
+    int resultado = a+b+c;
+        System.out.println("La suma da un total de "+resultado);
+    }
+    static class Coche{ //mi objeto coche tiene 4 puertas y se le pueden sumar
         int puertas = 4;
         public int incrementarPuertas(){
             this.puertas++;
             return puertas;
         }
     }
-
 }
